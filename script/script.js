13b9277d091d78e5dd2d28ca8eb73251e39f58b8
daveeedu <daveeedu@gmail.com>
2021-10-31 00:36:54 +0100

    new

--- script/script.js
+++ script/script.js
@@ -65,36 +65,36 @@
 
  function page(id) {
     
-     localStorage.setItem("postid", id);
+     localStorage.setItem("posts", id);
 
-     postId = localStorage.getItem('postid');
+     postId = localStorage.getItem("posts");
      console.log(postId);
 
      fetch(`https://jsonplaceholder.typicode.com/posts/${postId}`)
      .then(response => response.json())
      .then((e) => {
-         let postLayout = document.getElementById('post_layout')
-         let singlePost = "";
+         let postLayout = document.getElementById('post-layout')
+         let html = "";
 
-         singlePost = `
+         html = `
          <div class="col-md-8 mt-5 m-auto">
-                        <div class="card mb-8" onclick="page()">
-                            <div class="card-body">
-                                <div class="d-flex justify-content-end">
-                                    <h6 class= "text-danger">${e.id}</h6>
-                                </div>
-                                <h5 class="post-title mb-4 d-flex justify-content-center">${e.title} </h5>
-                                <p class="post-body text-center">${e.body}</p>
-                            </div>
-                           <div class="d-flex justify-content-center mb-3">
-                               <a href="index.htm"><button type="submit" class="btn btn-warning" >Home page</button></a>
-                           </div>
-                        </div>
+            <div class="card mb-8" onclick="page()">
+                <div class="card-body">
+                    <div class="d-flex justify-content-end">
+                        <h6 class= "text-danger">${e.id}</h6>
                     </div>
+                    <h5 class="post-title mb-4 d-flex justify-content-center">${e.title} </h5>
+                    <p class="post-body text-center">${e.body}</p>
+                </div>
+                <div class="d-flex justify-content-center mb-3">
+                    <a href="index.html"><button type="submit" class="btn btn-warning" >Home page</button></a>
+                </div>
+            </div>
+        </div>
          `
-         postLayout.innerHTML = singlePost
+         postLayout.innerHTML = html
      })
      
  }
 
-page()
+ 
